726204346a007e987d61150881ec7a82b4ed155b
jmunozc023 <jmunozc023@ulacit.ed.cr>
2025-02-01 01:16:44 -0600

    V1 de pagina completa
    Se agrega funcionalidad js
    se agregan botones de regreso para comodidad

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('foda-img').addEventListener('click', function() {
+    alert('FODA es un acrónimo que significa Fortalezas, Oportunidades, Debilidades y Amenazas. Es una herramienta de gestión que se utiliza para identificar los factores que influyen en el desempeño de una organización. ');
+});
+document.getElementById('org-img').addEventListener('click', function() {
+    alert('La estructura organizacional es del tipo horizontal, ya que se basa en la delegación de autoridad y responsabilidad en los empleados. Esto permite que los empleados tengan mayor autonomía y libertad para tomar decisiones. ');
+});
